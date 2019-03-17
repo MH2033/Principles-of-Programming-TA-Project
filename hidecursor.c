895305eff257df7469902cea3b61be00d03b3cfb
MH2033 <mh.akbare@gmail.com>
2019-03-17 19:45:33 +0330

    Added cursor hider

---
+++ hidecursor.c
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+void hidecursor()
+{
+   HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+   CONSOLE_CURSOR_INFO info;
+   info.dwSize = 100;
+   info.bVisible = FALSE;
+   SetConsoleCursorInfo(consoleHandle, &info);
+}
