85078bfa896338cb561582d0492de55c1793baf1
MH2033 <mh.akbare@gmail.com>
2019-03-17 20:06:18 +0330

    Creating a variable that saves the config files

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include "read_file.h"
+
+char config[100];
 
 int main() {
+    read_from_file(config, "test_case_1.txt");
     return 0;
 }
--- read_file.h
+++ read_file.h
@@ -2,7 +2,6 @@
 
 void read_from_file(char file_data[], char file_name[]);
 
-``
 // the function will store file's data in file_data
 
 // file_name -> e.g., "test_case_1.txt"
