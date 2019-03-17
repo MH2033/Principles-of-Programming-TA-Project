c6f9f24526067649014e03b31fb2048127642077
MH2033 <mh.akbare@gmail.com>
2019-03-17 19:41:44 +0330

    Added welcome screen

---
+++ welcome_screen.c
@@ -0,0 +1,11 @@
+int welcome_screen(void)
+{
+	int mode;
+	printf("\n 1. Player 1 vs Player 2");
+	printf("\n 2. Player 1 vs Computer");
+	printf("\n\n Enter a number: ");
+	if((mode = getchar()) == '1')
+		return 1;
+	else
+		return 2;
+}
