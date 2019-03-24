9948dc36832d5d3eea8840e857aaed6e7386efef
MH2033 <mh.akbare@gmail.com>
2019-03-24 13:53:19 +0430

    Change config to a local variable

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include "prototypes.h"
 
-char config[100];
-
 int main() {
     int ans;
+    char config[100];
     read_from_file(config, "test_case_1.txt");
     ans = welcome_screen();
     return 0;
