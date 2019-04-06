42bfaaacafb344db9e89c4d871fd7318dbdfa2f7
pooya <pooyafekri79@gmail.com>
2019-04-06 16:51:59 +0430

    making game primitive map

--- game_map.c
+++ game_map.c
@@ -2,8 +2,11 @@ void game_map_making(char game_map [][100],int x,int y){
     int i,j;
     for (i = 0; i < x; i++) {
         for (j = 0; j < y;j++) {
-            game_map[i][j] = ' ';
+            if(i ==0 || j == 0 || i== x-1 || j == y-1){
+                game_map[i][j] = '!';
+            } else {
+                game_map[i][j] = ' ';
+            }
         }
     }
-    game_map[i+1][j+1] = '\0';
 }
