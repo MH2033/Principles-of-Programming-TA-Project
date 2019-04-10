e30c3532f5ede1ad78501bd8fb58b7c08203e176
MH2033 <mh.akbare@gmail.com>
2019-04-10 18:40:02 +0430

    Showing score to players

--- print_map.c
+++ print_map.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int x,y;
 char game_map[101][101];
+extern int score1, score2;
 void print_map(void){
     for (int i = 0; i <= x; ++i) {
         printf("\n");
@@ -8,4 +9,7 @@ void print_map(void){
             printf("%c",game_map[i][j]);
         }
     }
+    printf("\n\nPlayer 1 score: %d", score1);
+    printf("\nPlayer 2 score: %d", score2);
+    //printf("Remaining time: %f", time);
 }
