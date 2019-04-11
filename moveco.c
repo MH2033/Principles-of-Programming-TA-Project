f20506dda5733b8c7416048474656a5672b9be30
MH2033 <mh.akbare@gmail.com>
2019-04-11 13:56:30 +0430

    Fixed hanging while A.I would hit block or teleport

--- moveco.c
+++ moveco.c
@@ -5,16 +5,24 @@ int moveco(int i,int j) {
     if ((mx = i - x_p_x) < 0) {
         if(game[x_p_x-1][x_p_y] == 0 || game[x_p_x-1][x_p_y] == 1 || game[x_p_x-1][x_p_y] == 5)
             c2 = 'i';
+        else
+            c2 = 'l';
     } else if ((mx = i - x_p_x) > 0) {
         if (game[x_p_x + 1][x_p_y] == 0 || game[x_p_x + 1][x_p_y] == 1 || game[x_p_x + 1][x_p_y] == 5)
             c2 = 'k';
+        else
+            c2 = 'l';
     }
     if ((my = j - x_p_y) < 0) {
         if(game[x_p_x][x_p_y-1] == 0 || game[x_p_x][x_p_y-1] == 1 || game[x_p_x][x_p_y-1] == 5)
             c2 ='j';
+        else
+            c2 = 'i';
     } else if ((my = j - x_p_y) > 0) {
         if(game[x_p_x][x_p_y+1] == 0 || game[x_p_x][x_p_y+1] == 1 || game[x_p_x][x_p_y+1] == 5)
             c2 = 'l';
+        else
+            c2 = 'k';
     }
     return c2;
 }
