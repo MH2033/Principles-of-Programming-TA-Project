e011aa153ed3ce38a3c9ff314e9a4771e7251be2
pooya <pooyafekri79@gmail.com>
2019-04-12 22:12:38 +0430

    change range of a\i

--- FindCloseFood.c
+++ FindCloseFood.c
@@ -1,14 +1,14 @@
 #include <math.h>
 #include "prototypes.h"
 #define LEN 100
-extern int x_p_x,x_p_y;
+extern int x_p_x,x_p_y,x,y;
 extern int game[LEN][LEN];
 int FindCloseFood(void) {
     int i, j, cdotx, cdoty, cbigdotx, cbigdoty, firstdot = 1, firstbigdot = 1;
     double dist1, dist2, weightdot, weightbigdot;
-    for (i = x_p_x - 40; i < x_p_x +40; i++) {
-        for (j = x_p_y - 40; j < x_p_y + 40; j++) {
-            if(i >0 && j > 0) {
+    for (i = x_p_x - x; i < x_p_x + x; i++) {
+        for (j = x_p_y - y; j < x_p_y + y; j++) {
+            if(i >0 && j > 0 && i<x && j<y) {
                 if (game[i][j] == 1) {
                     if (firstdot) {
                         cdotx = i;
