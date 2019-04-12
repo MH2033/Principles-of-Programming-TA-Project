a8c2c5a7c33b06796e372e14e8a2c9347865fd3f
MH2033 <mh.akbare@gmail.com>
2019-04-12 19:30:19 +0430

    Now user should press Enter instead of any key to exit

--- ShowWinner.c
+++ ShowWinner.c
@@ -7,11 +7,12 @@ void ShowWinner(void){
     system("CLS");
     printf("\n Player 1: %d\n Player 2: %d", score1, score2);
     if (score1 > score2)
-        printf("\n\n Player 1 has won!\n\n Press any key to quit the game...");
+        printf("\n\n Player 1 has won!\n\n Press Enter to quit the game...");
     else if (score1 == score2) {
-        printf("\n\n Draw!\n\n Press any key to quit the game...");
+        printf("\n\n Draw!\n\n Press Enter to quit the game...");
     } else {
-        printf("\n\n Player 2 has won!\n\n Press any key to quit the game...");
+        printf("\n\n Player 2 has won!\n\n Press Enter to quit the game...");
     }
-    getch();
+    while(getch() != '\r')
+        ;
 }
