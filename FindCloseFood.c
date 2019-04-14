b3e46bf2ca8a75842de55a594cd9dbbf5a6d0fd8
pooya <pooyafekri79@gmail.com>
2019-04-14 15:54:32 +0430

    some change

--- FindCloseFood.c
+++ FindCloseFood.c
@@ -6,8 +6,8 @@ extern int game[LEN][LEN];
 int FindCloseFood(void) {
     int i, j, cdotx, cdoty, cbigdotx, cbigdoty, firstdot = 1, firstbigdot = 1;
     double dist1, dist2, weightdot, weightbigdot;
-    for (i = x_p_x - x; i < x_p_x + x; i++) {
-        for (j = x_p_y - y; j < x_p_y + y; j++) {
+    for (i = x_p_x - 10; i < x_p_x + 10; i++) {
+        for (j = x_p_y - 10; j < x_p_y + 10; j++) {
             if(i >0 && j > 0 && i<x && j<y) {
                 if (game[i][j] == 1) {
                     if (firstdot) {
@@ -31,6 +31,35 @@ int FindCloseFood(void) {
                 }
             }
         }
+    }
+    if(firstbigdot == 1 && firstdot == 1){
+        for (i = x_p_x - x; i < x_p_x + x; i++) {
+            for (j = x_p_y - y; j < x_p_y + y; j++) {
+                if(i >0 && j > 0 && i<x && j<y) {
+                    if (game[i][j] == 1) {
+                        if (firstdot) {
+                            cdotx = i;
+                            cdoty = j;
+                            firstdot = 0;
+                        } else {
+                            dist1 = sqrt(pow((cdotx - x_p_x), 2) + pow((cdoty - x_p_y), 2));
+                            dist2 = sqrt(pow((i - x_p_x), 2) + pow((j - x_p_y), 2));
+                            if (dist2 < dist1) {
+                                cdotx = i;
+                                cdoty = j;
+                            }
+                        }
+                    } else if (game[i][j] == 5) {
+                        if (firstbigdot) {
+                            cbigdotx = i;
+                            cbigdoty = j;
+                            firstbigdot = 0;
+                        }
+                    }
+                }
+            }
+        }
+
     }
     weightdot = 1.0 / sqrt(pow((cdotx - x_p_x), 2) + pow((cdoty - x_p_y), 2));
     weightbigdot = 5.0 / sqrt(pow((cbigdotx - x_p_x), 2) + pow((cbigdoty - x_p_y), 2));
