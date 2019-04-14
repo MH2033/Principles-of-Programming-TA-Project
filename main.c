65047ddda749d76c04c960264e5bf3cd88fe48cf
pooya <pooyafekri79@gmail.com>
2019-04-14 17:21:41 +0430

    SOME CHANGE

--- main.c
+++ main.c
@@ -8,14 +8,13 @@ int flagx_i = 0,flagx_k = 0,flagy_l = 0,flagy_j = 0,dots, score1 = 0,score2 = 0,
 int x, y ,game[LEN][LEN] = {0},TX[2]={0},TY[2] = {0};
 int randomness = 0;
 double times;
-int game_map[101][101];
+int game_map[LEN][LEN];
 int main() {
     int ans;
     char config[LEN];
     ans = welcome_screen();
     read_from_file(config, "test_case_1.txt");
     load_config(config);
-    game_map[x][y] = '\0';
     hidecursor();
     if(ans)
         mode_1();
