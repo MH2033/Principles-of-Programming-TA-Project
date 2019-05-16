ed73945236693649fd132ade12efd46c8046438c
MH2033 <mh.akbare@gmail.com>
2019-05-16 16:45:21 +0430

    Fix showing elements of map at the same place on each run.

--- main.c
+++ main.c
@@ -6,11 +6,13 @@
 #define LEN 100
 int flagx_i = 0,flagx_k = 0,flagy_l = 0,flagy_j = 0,dots, score1 = 0,score2 = 0,x,y,x_p_x,x_p_y,o_p_y,o_p_x,score[2] = {0};
 int x, y ,game[LEN][LEN] = {0},TX[2]={0},TY[2] = {0};
-int randomness = 0;
+int randomness;
 int mode;
 double times;
 int game_map[LEN][LEN];
 int main() {
+    srand(time(0));
+    randomness = rand();
     char config[LEN];
     mode = welcome_screen();
     read_from_file(config, "test_case_1.txt");
