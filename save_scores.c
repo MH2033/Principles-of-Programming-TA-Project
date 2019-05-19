3e6e7e6a78887e87905f161814c7712cbdf3de9d
MH2033 <mh.akbare@gmail.com>
2019-05-20 00:07:37 +0430

    Fix leaderboard

--- save_scores.c
+++ save_scores.c
@@ -28,84 +28,84 @@ void save_scores(){
     fclose(file);
     i = 0;
     while(content[i] != NULL){
-        if((p1 = strstr(content[i], p1name)) != NULL && (p2 = strstr(content[i], p2name)) != NULL){
-            flag = 1;
-            if(score1 > score2){
-                if(p1 == content[i]){
-                    while(*p1 != '-'){
+        if((p1 = strstr(content[i], p1name)) != NULL && (p2 = strstr(content[i], p2name)) != NULL) {
+            if ((*(p1 + strlen(p1name)) == '\t' || *(p1 + strlen(p1name)) == 0) &&
+                (*(p2 + strlen(p2name)) == '\t' || *(p2 + strlen(p2name)) == 0)) {
+                flag = 1;
+                if (score1 > score2) {
+                    if (p1 == content[i]) {
+                        while (*p1 != '-') {
+                            p1++;
+                        }
+                        p1 -= 2;
+                        while (*p1 != '\t') {
+                            p1--;
+                        }
                         p1++;
-                    }
-                    p1 -= 2;
-                    while(*p1 != '\t'){
-                        p1--;
-                    }
-                    p1++;
-                    while(*p1 != '\t'){
-                        s1 = s1 * 10 + *p1- '0';
+                        while (*p1 != '\t') {
+                            s1 = s1 * 10 + *p1 - '0';
+                            p1++;
+                        }
+                        s1++;
+                        temp = (char *) malloc(sizeof(char) * 1000);
+                        sprintf(temp, "%s\t%d%s", p1name, s1, p1);
+                        content[i] = temp;
+                    } else {
+                        p1 -= 2;
+                        while (*p1 != '\t') {
+                            p1--;
+                        }
                         p1++;
+                        while (*p1 != '\t') {
+                            s1 = s1 * 10 + *p1 - '0';
+                            p1++;
+                        }
+                        s1++;
+                        j = 0;
+                        while (content[i][j] != '-')
+                            j++;
+                        content[i][j] = 0;
+                        temp = (char *) malloc(sizeof(char) * 1000);
+                        sprintf(temp, "%s-\t%d\t%s", content[i], s1, p1name);
+                        content[i] = temp;
                     }
-                    s1++;
-                    temp = (char *)malloc(sizeof(char)* 1000);
-                    sprintf(temp, "%s\t%d%s", p1name, s1, p1);
-                    content[i] = temp;
-                }
-                else{
-                    p1 -= 2;
-                    while(*p1 != '\t'){
-                        p1--;
-                    }
-                    p1++;
-                    while(*p1 != '\t'){
-                        s1 = s1 * 10 + *p1- '0';
-                        p1++;
-                    }
-                    s1++;
-                    j = 0;
-                    while(content[i][j] != '-')
-                        j++;
-                    content[i][j] = 0;
-                    temp = (char *)malloc(sizeof(char)* 1000);
-                    sprintf(temp, "%s-\t%d\t%s", content[i], s1, p1name);
-                    content[i] = temp;
-                }
-            }
-            else if(score2 > score1){
-                if(p2 == content[i]){
-                    while(*p2 != '-'){
+                } else if (score2 > score1) {
+                    if (p2 == content[i]) {
+                        while (*p2 != '-') {
+                            p2++;
+                        }
+                        p2 -= 2;
+                        while (*p2 != '\t') {
+                            p2--;
+                        }
                         p2++;
-                    }
-                    p2 -= 2;
-                    while(*p2 != '\t'){
-                        p2--;
-                    }
-                    p2++;
-                    while(*p2 != '\t'){
-                        s2 = s2 * 10 + *p2- '0';
+                        while (*p2 != '\t') {
+                            s2 = s2 * 10 + *p2 - '0';
+                            p2++;
+                        }
+                        s2++;
+                        temp = (char *) malloc(sizeof(char) * 1000);
+                        sprintf(temp, "%s\t%d%s", p2name, s2, p2);
+                        content[i] = temp;
+                    } else {
+                        p2 -= 2;
+                        while (*p2 != '\t') {
+                            p2--;
+                        }
                         p2++;
+                        while (*p2 != '\t') {
+                            s2 = s2 * 10 + *p2 - '0';
+                            p2++;
+                        }
+                        s2++;
+                        j = 0;
+                        while (content[i][j] != '-')
+                            j++;
+                        content[i][j] = 0;
+                        temp = (char *) malloc(sizeof(char) * 1000);
+                        sprintf(temp, "%s-\t%d\t%s", content[i], s2, p2name);
+                        content[i] = temp;
                     }
-                    s2++;
-                    temp = (char *)malloc(sizeof(char)* 1000);
-                    sprintf(temp, "%s\t%d%s", p2name, s2, p2);
-                    content[i] = temp;
-                }
-                else{
-                    p2 -= 2;
-                    while(*p2 != '\t'){
-                        p2--;
-                    }
-                    p2++;
-                    while(*p2 != '\t'){
-                        s2 = s2 * 10 + *p2- '0';
-                        p2++;
-                    }
-                    s2++;
-                    j = 0;
-                    while(content[i][j] != '-')
-                        j++;
-                    content[i][j] = 0;
-                    temp = (char *)malloc(sizeof(char)* 1000);
-                    sprintf(temp, "%s-\t%d\t%s", content[i], s2, p2name);
-                    content[i] = temp;
                 }
             }
         }
